94168f62a85c85cb9a7d2b0b657fbe78ed1d8649
ycbingpo <575781252@qq.com>
2019-03-08 09:49:04 +0800

    Chapter6

--- Chapter6/src/main/java/com/tester/demo/TestMehodsDemo.java
+++ Chapter6/src/main/java/com/tester/demo/TestMehodsDemo.java
@@ -1,4 +1,25 @@
 package com.tester.demo;
 
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class TestMehodsDemo {
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,2);
+    }
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+    }
+    @Test
+    public void test3(){
+        Assert.assertEquals("aaaa","aaaa");
+    }
+    @Test
+    public void logDemo(){
+        Reporter.log("这是我们自己的运行异常");
+        throw new RuntimeException("运行异常");
+    }
 }
